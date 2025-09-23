5545a0c6139b088404ae2f2bc008df0e449fe592
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-09-23 14:05:19 +0300

    Update search.tsx

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,3 +1,4 @@
+import { ShikimoriAnime } from "@/API/Shikimori/Shikimori.types";
 import { ContextMenu } from "@/components/ContextComponent";
 import { GradientBlur } from "@/components/GradientBlur";
 import AnimeItem from "@/components/Screens/Search/AnimeItem";
@@ -5,23 +6,25 @@ import { EmptyPlaceholder } from "@/components/Screens/Search/EmptyPlaceholder";
 import { ModalFilter } from "@/components/Screens/Search/Filters";
 import Input from "@/components/Screens/Search/Input";
 import Toast from "@/components/ui/Toast";
+import { useBottomHeight } from "@/hooks/useBottomHeight";
 import { auth } from "@/lib/firebase";
+import { addFavoriteAnime } from "@/lib/firebase/userFavorites";
 import { useSearchStore } from "@/store/filterStore";
-import { addFavoriteAnime } from "@/utils/firebase/userFavorites";
 import { FlashList, FlashListRef } from "@shopify/flash-list";
 import { LinearGradient } from "expo-linear-gradient";
-import { useEffect, useRef, useState } from "react";
-import { ActivityIndicator, Dimensions, Keyboard, StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { ActivityIndicator, Dimensions, Keyboard, Platform, StyleSheet, View } from "react-native";
 import { easeGradient } from "react-native-easing-gradient";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { SFSymbols6_0 } from "sf-symbols-typescript";
 import { useShallow } from "zustand/shallow";
 
 const { width, height } = Dimensions.get("screen");
+const isIOS26 = Platform.Version >= '26.0'
 
 export default function SearchScreen() {
     const insets = useSafeAreaInsets();
-    // const bottomTabHeight = useBottomTabBarHeight();
+    const bottomTabHeight = useBottomHeight();
 
     const [showFilters, setShowFilters] = useState(false);
 
@@ -59,14 +62,11 @@ export default function SearchScreen() {
     })))
 
     useEffect(() => {
-        console.log(query)
         if (!query || query.length === 0) {
-            return
+            reset()
+            fetchResults(true);
+            return;
         }
-        // if (!query) {
-        //     reset(); // очищаем результаты только при очистке query
-        //     return;
-        // }
 
         const handler = setTimeout(() => {
             reset();
@@ -78,9 +78,7 @@ export default function SearchScreen() {
 
     useEffect(() => { fetchResults() }, [])
 
-    const handleEndReached = () => {
-        fetchResults(false);
-    };
+    const handleEndReached = () => fetchResults(false);
 
     const handleApplyFilters = () => {
         setShowFilters(false);
@@ -89,7 +87,7 @@ export default function SearchScreen() {
         ref.current?.scrollToOffset({ offset: 0, animated: true });
     };
 
-    const renderItem = ({ item, index }: any) => (
+    const renderItem = useCallback(({ item, index }: { item: ShikimoriAnime; index: number }) => (
         <View style={{ paddingVertical: 5 }}>
             <ContextMenu
                 triggerItem={<AnimeItem item={item} index={index} />}
@@ -104,15 +102,13 @@ export default function SearchScreen() {
                 }]}
             />
         </View>
-    );
+    ), []);
 
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: "#1b1919"
-        }}>
+        <View style={{ flex: 1, backgroundColor: 'black' }}>
             <View style={{ top: insets.top }}>
-                <Toast show={showToast.show}
+                <Toast
+                    show={showToast.show}
                     text={showToast.text}
                     iconSize={18}
                     iconColor={showToast.color}
@@ -121,76 +117,47 @@ export default function SearchScreen() {
                 />
             </View>
 
-            <LinearGradient
-                colors={["black", "rgba(0,0,0,0.5)", "rgba(0,0,0,0)"]}
-                style={[StyleSheet.absoluteFill, { width: "100%", height: insets.top * 3, zIndex: 200 }]}
-            />
-            <GradientBlur
-                containerStyle={{
-                    position: 'absolute',
-                    top: 0,
-                    zIndex: 2, width, height: insets.top * 3.2,
-                }}
-                locations={locations as [number, number, ...number[]]}
-                colors={colors as [string, string, ...string[]]}
-                tint="regular"
-                blurIntensity={50}
-            />
+            <>
+                <LinearGradient
+                    colors={["black", "rgba(0,0,0,0.5)", "rgba(0,0,0,0)"]}
+                    style={[StyleSheet.absoluteFill, { width: "100%", height: insets.top * 2.65, zIndex: 200 }]}
+                />
+                <GradientBlur
+                    containerStyle={{
+                        position: 'absolute',
+                        top: 0,
+                        zIndex: 2, width, height: insets.top * 2.65,
+                    }}
+                    locations={locations as [number, number, ...number[]]}
+                    colors={colors as [string, string, ...string[]]}
+                    tint="regular"
+                    blurIntensity={50}
+                />
+            </>
 
             <Input onOpenFilter={setShowFilters} />
 
-            {!isLoading && !results.length && (
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        backgroundColor: "rgba(0,0,0,0.7)",
-                        zIndex: 100,
-                    }}
-                >
-                    <LinearGradient
-                        colors={["#ff7e5f", "#feb47b", "#86a8e7", "#91eac9"]}
-                        style={{
-                            width: 80,
-                            height: 80,
-                            borderRadius: 40,
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 1 }}
-                    >
-                        <ActivityIndicator size="large" color="#fff" />
-                    </LinearGradient>
-                    <Text style={{ color: "#fff", marginTop: 18, fontSize: 18, fontWeight: "600" }}>
-                        Поиск аниме...
-                    </Text>
-                </View>
-            )}
-
             <FlashList
                 ref={ref}
-                style={{ flex: 1 }}
-                contentContainerStyle={{ paddingHorizontal: 10, paddingTop: insets.top + 80, 
-                    // paddingBottom: bottomTabHeight + 30 
-                }}
-                scrollIndicatorInsets={{ top: 65 }}
                 data={results}
-                keyExtractor={(item: any) => `anime-${item.id}`}
+                keyExtractor={(item: ShikimoriAnime) => `anime-${item.malId}`}
                 renderItem={renderItem}
+
+                style={{ flex: 1 }}
+                contentContainerStyle={{
+                    paddingHorizontal: 10,
+                    paddingTop: isIOS26 ? insets.top + 10 : insets.top + 30,
+                    paddingBottom: isIOS26 ? 0 : bottomTabHeight,
+                }}
+                scrollIndicatorInsets={{ top: 65, bottom: bottomTabHeight }}
                 onScroll={() => Keyboard.dismiss()}
                 scrollEventThrottle={16}
                 onEndReached={handleEndReached}
                 onEndReachedThreshold={0.86}
+                contentInsetAdjustmentBehavior='automatic'
                 ListFooterComponent={
                     isLoading ?
-                        <ActivityIndicator size="small" color="#fff" />
+                        <ActivityIndicator size="small" color="#fff" style={{ height: results.length < 1 ? height : undefined, justifyContent: 'center', alignItems: 'center', top: results.length < 1 ? -150 : undefined }} />
                         : null
                 }
                 ListEmptyComponent={!isLoading ? (<EmptyPlaceholder />) : null}
