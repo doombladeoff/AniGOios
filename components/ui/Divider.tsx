07704d859ec7bef6530d211041678007d846326c
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-11-01 16:51:02 +0200

    Add Divider

---
+++ components/ui/Divider.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, View } from "react-native";
+
+export const Divider = () => {
+    return <View style={{ height: StyleSheet.hairlineWidth, backgroundColor: 'rgba(100,100,100, 0.5)', width: '100%' }} />;
+};
